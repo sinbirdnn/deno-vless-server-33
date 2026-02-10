9b834e40e438db6391280fa68d8c0ee5d589bc3a
sinbirdnn <sinbirdnn@outlook.com>
2026-02-10 16:55:15 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '88aeb75a-6627-4c0a-b5de-2b88426dfabe'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
